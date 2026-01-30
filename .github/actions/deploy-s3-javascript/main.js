916a90bfbf8bf8e0a7d021889a67a690e3d7e184
abhinavcloud <abhinav@abhinav-cloud.com>
2026-01-30 12:29:07 +0530

    added static website

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,10 +1,19 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+
+    //Get Input Values
     core.notice('Hello from my custom java script action')
-        
+    const bucket = core.getInput('bucket', {required: true});
+    const region = core.getInput('region', {required: true});
+    const dist_folder = core.getInput('dist_folder', {required: true});
+    const s3url = `s3://${bucket}`
+    
+    //Upload the files
+    
+    exec.exec(`aws s3 sync ${dist-folder} ${s3url} --region ${region}`)
 }
 
 run();
