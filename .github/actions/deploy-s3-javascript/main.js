7e33399dfa644f432cf85e4673fa4e6be7dfaf9a
abhinavcloud <abhinav@abhinav-cloud.com>
2026-01-30 18:00:43 +0530

    fixed main

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,18 +2,24 @@ const core = require('@actions/core');
 // const github = require('@actions/github');
 const exec = require('@actions/exec');
 
-function run() {
+async function run() {
+  try {
+    core.notice('Hello from my custom javascript action');
 
-    //Get Input Values
-    core.notice('Hello from my custom java script action')
-    const bucket = core.getInput('bucket', {required: true});
-    const region = core.getInput('region', {required: true});
-    const dist_folder = core.getInput('dist_folder', {required: true});
-    const s3url = `s3://${bucket}`
-    
-    //Upload the files
-    
-    exec.exec(`aws s3 sync ${dist_folder} ${s3url} --region ${region}`)
+    const bucket = core.getInput('bucket', { required: true });
+    const region = core.getInput('region', { required: true });
+    const dist_folder = core.getInput('dist_folder', { required: true });
+
+    const s3url = `s3://${bucket}`;
+
+    await exec.exec(
+      `aws s3 sync ${dist_folder} ${s3url} --region ${region}`
+    );
+
+    core.notice('S3 sync completed successfully');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run();
